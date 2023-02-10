af3430bbb50524d0bb36d8eb688ccdbab5567520
Alfredo Capasso <alfredocapasso@AirsomtrAlfredo.lan>
2023-02-10 07:58:15 +0100

    env added

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-const API_BASE = "";
+const API_BASE = process.env.REACT_APP_API_BASE;
 const URI_PREFIX = "api/v1";
 const USER_PREFIX = `${API_BASE}/${URI_PREFIX}/user`;
 const GEO_PREFIX = `${API_BASE}/${URI_PREFIX}/geo`;
